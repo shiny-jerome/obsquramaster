23978477ec9c49ade33d59e2d2cca3d3dfa07ad0
shiny-jerome <shiny.j22@gmail.com>
2023-03-02 21:34:19 +0530

    created folders

---
+++ src/main/java/sherin/HelloWorld.java
@@ -0,0 +1,10 @@
+package sherin;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/vishnu/HelloWorld.java
@@ -0,0 +1,10 @@
+package vishnu;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
