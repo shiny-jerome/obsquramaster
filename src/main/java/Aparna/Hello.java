a7f560c2a0d503fd8eb4978393fdd6aa588f5297
aparnamerlaa <aparnamerlaa@outlook.com>
2023-04-26 22:47:12 +0400

    my second commit

---
+++ src/main/java/Aparna/Hello.java
@@ -0,0 +1,10 @@
+package Aparna;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("dfdsgfsg");
+
+	}
+
+}
--- src/main/java/Aparna/HelloWorld.java
+++ src/main/java/Aparna/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 	System.out.println("hi  heloooo");
+	System.out.println("hi  ");
+	System.out.println("heloooo");
 
 	}
 
