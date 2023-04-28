2505e3396f85fce6fa0c6358f9539d9fe725455e
imathegk <gopikrishnanvtvm@gmail.com>
2023-04-28 20:07:58 +0530

    3 new programs commiting

---
+++ src/main/java/Gopikrishnan/triangle.java
@@ -0,0 +1,15 @@
+package Gopikrishnan;
+
+public class triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int height=10;
+		int bredth=20;
+		int mul=height*bredth;
+		int area=mul/2;
+		System.out.println(area);
+
+	}
+
+}
