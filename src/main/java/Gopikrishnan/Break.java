ba28e19702607b94f8601903890abcae1a0bd5cf
shiny-jerome <111281264+shiny-jerome@users.noreply.github.com>
2023-12-04 09:35:41 +0530

    Update Break.java

--- src/main/java/Gopikrishnan/Break.java
+++ src/main/java/Gopikrishnan/Break.java
@@ -6,7 +6,7 @@ public class Break {
 		// TODO Auto-generated method stub
 		for(int i=1;i<=20;i++) {
 			if(i==10) {
-				System.out.println("completed");
+				//System.out.println("completed");
 			}
 			
 			
